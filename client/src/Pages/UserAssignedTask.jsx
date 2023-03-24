efa2264bb42b009ebb74899e47759984f1616c27
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-03-24 18:59:05 +0530

    chart.js

--- client/src/Pages/UserAssignedTask.jsx
+++ client/src/Pages/UserAssignedTask.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { getTasks } from '../redux/TaskManager/task.action';
 import { BiLoaderCircle } from "react-icons/bi";
+import { Line, Pie } from 'react-chartjs-2';
+import { Chart } from 'chart.js/auto';
 
 const UserAssignedTask = () => {
     const { loading, error, tasks } = useSelector((store) => store.taskManager);
@@ -25,6 +27,54 @@ const UserAssignedTask = () => {
         };
     }, [tasks]);
 
+
+    let total_Todo = 100, total_Progress = 100, total_Done = 100;
+    for (let ele of assignedTask) {
+        if (ele.status == "Todo") {
+            total_Todo++;
+        } else if (ele.status == "Progress") {
+            total_Progress++;
+        } else {
+            total_Done++;
+        }
+    }
+    
+    let status = ["Todo", "Progress", "Done"];
+    let No_of_status = [total_Todo, total_Progress, total_Done];
+
+    var chart = {
+        labels: status.map((ele) => {
+            return ele;
+        }),
+        datasets: [
+            {
+                label: "Status of Tasks",
+                data: status.map((ele) => {
+                    return ele;
+                }),
+                backgroundColor: createColor(255)
+            },
+            {
+                label: "Number of Tasks according to status",
+                data: No_of_status.map((ele) => {
+                    return ele;
+                }),
+                backgroundColor: createColor(255)
+            }
+        ],
+        borderColor: "black",
+        borderWidth: 1
+    };
+    const [chartStatus, setChartStatus] = useState(chart);
+
+    
+
+    function random(num) {
+        return Math.floor(Math.random() * num);
+    };
+    function createColor(num) {
+        return `rgb(${random(num)}, ${random(num)}, ${random(num)})`;
+    };
 
     return (
         <Box>
@@ -58,9 +108,85 @@ const UserAssignedTask = () => {
                 </Box>
             </Box>}
 
+            <Box ml={{ base: 0, sm: "10%", md: "20%", xl: "23%", "2xl": "23%" }} w="500px" mt={"5%"} display="flex" gap={"20px"} flexDirection={{ base: "column", sm: "column", md: "column", xl: "row", "2xl": "row" }}>
+                <Box><Line data={chartStatus} /></Box>
+                <Box><Pie data={chartStatus} /></Box>
+            </Box>
 
         </Box>
     );
 }
 
 export default UserAssignedTask;
+
+
+
+
+
+
+// import { Box, Img, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+// import React, { useEffect, useState } from 'react'
+// import { Bar, Line, Pie } from 'react-chartjs-2';
+// import { Chart } from 'chart.js/auto';
+
+// const Portfolio = () => {
+//     const [buyStock, setBuyStock] = useState([]);
+//     useEffect(() => {
+//         getBuyStock();
+//     }, []);
+
+//     const getBuyStock = async () => {
+//         try {
+//             let res = await fetch(`https://render-si4e.onrender.com/buyStock`);
+//             res = await res.json();
+//             if (res !== undefined) {
+//                 setBuyStock(res)
+//             }
+//         } catch (err) {
+//             console.log(err);
+//         }
+//     };
+
+//     console.log(buyStock);
+
+//     var chart = {
+//         labels: buyStock.map((ele) => {
+//             return ele.total_shares
+//         }),
+//         datasets: [
+//             {
+//                 label: "Company Name",
+//                 data: buyStock.map((ele) => {
+//                     return ele.company_name
+//                 }),
+//                 backgroundColor: "rgb(255, 45, 54)"
+//             },
+//             {
+//                 label: "cost_per_share",
+//                 data: buyStock.map((ele) => {
+//                     return ele.cost_per_share
+//                 }),
+//                 backgroundColor: "rgb(255, 45, 54)"
+//             }
+//         ],
+//         borderColor: "black",
+//         borderWidth: 1
+//     };
+//     const [chartStock, setChartStock] = useState(chart);
+
+//     var total_shares_of_user = 0;
+//     for (let ele of buyStock) {
+//         total_shares_of_user += ele.total_shares;
+//     };
+
+//     return (
+//         <Box>
+//             <Box w="500px">
+//                 <Line data={chartStock} />
+//                 <Pie data={chartStock} />
+//             </Box>
+//         </Box>
+//     );
+// };
+
+// export default Portfolio;
